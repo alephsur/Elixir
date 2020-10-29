c19d014e6d6b32743160264fd9ffea4661a56244
David Franco <david.franco@ingrammicro.com>
2020-10-29 18:50:08 +0100

    String Series

---
+++ series/lib/string_series.ex
@@ -0,0 +1,29 @@
+defmodule StringSeries do
+  @doc """
+  Given a string `s` and a positive integer `size`, return all substrings
+  of that size. If `size` is greater than the length of `s`, or less than 1,
+  return an empty list.
+  """
+  @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+  def slices(_s, _size) do
+    split(to_charlist(_s), _size, [])
+  end
+
+  defp split([],_,series) do
+    Enum.reverse(series)
+  end
+
+  defp split([head|tail], size, series) when size <= 0 do
+    []
+  end
+
+  defp split([head|tail], size, series) do
+    element = [head|tail] |> Enum.split(size) |> elem(0)
+
+    if length(element) < size do
+      split(tail, size,  series)
+    else
+      split(tail, size,  [to_string(element) | series])
+    end
+  end
+end
