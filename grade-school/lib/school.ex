01bf6c72e4d2f196c7f2809606b53ed2efb08770
alephsur <david.franco.gutierrez@gmail.com>
2021-03-23 23:13:27 +0100

    grade School
    first version

--- grade-school/lib/school.ex
+++ grade-school/lib/school.ex
@@ -10,6 +10,11 @@ defmodule School do
   """
   @spec add(map, String.t(), integer) :: map
   def add(db, name, grade) do
+    cond do
+      Map.fetch(db, grade) == :error -> Map.put(db,grade, [name])
+      true -> Map.put(db,grade, Map.fetch!(db, grade) ++ [name])
+    end
+
   end
 
   @doc """
@@ -17,6 +22,10 @@ defmodule School do
   """
   @spec grade(map, integer) :: [String.t()]
   def grade(db, grade) do
+    cond do
+      Map.fetch(db, grade) == :error -> []
+      true -> Map.fetch!(db,grade)
+    end
   end
 
   @doc """
@@ -24,5 +33,16 @@ defmodule School do
   """
   @spec sort(map) :: [{integer, [String.t()]}]
   def sort(db) do
+    get_sorted_list(Map.keys(db), db, [])
   end
+
+  def get_sorted_list([], _, lista) do
+    lista
+  end
+
+  def get_sorted_list([head | tail], map, lista) do
+    get_sorted_list(tail, map, lista ++ [{head, Enum.sort(Map.fetch!(map, head))}])
+  end
+
+
 end
