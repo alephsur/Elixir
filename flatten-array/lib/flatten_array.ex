d1cd7d074a6f553903dd59a43edab11554183f89
alephsur <david.franco.gutierrez@gmail.com>
2021-04-30 19:20:41 +0200

    flatter_array
    recursive solution

--- flatten-array/lib/flatten_array.ex
+++ flatten-array/lib/flatten_array.ex
@@ -14,7 +14,18 @@ defmodule FlattenArray do
 
   @spec flatten(list) :: list
   def flatten(list) do
-    List.flatten(list)
-    |> Enum.filter(fn x -> x != nil end)
+    process_list(list, [])
+  end
+
+  def process_list([], acc_list) do
+    acc_list
+  end
+
+  def process_list([head|tail], acc_list) do
+    cond do
+      is_list(head) -> process_list(tail, process_list(head, acc_list))
+      head == nil -> process_list(tail, acc_list)
+      true -> process_list(tail, acc_list ++ [head])
+    end
   end
 end
