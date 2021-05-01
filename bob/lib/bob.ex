fa736dda5ee2307acc6685058cb0c4b5c579c84d
alephsur <david.franco.gutierrez@gmail.com>
2021-05-01 18:23:26 +0200

    Bob
    Improve the version

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -2,18 +2,18 @@ defmodule Bob do
   def hey(input) do
     cond do
       String.trim(input) == "" -> "Fine. Be that way!"
-      is_screeming(String.trim(input)) and is_question(String.trim(input)) -> "Calm down, I know what I'm doing!"
-      is_screeming(String.trim(input)) -> "Whoa, chill out!"
-      is_question(String.trim(input)) -> "Sure."
+      screeming?(String.trim(input)) and question?(String.trim(input)) -> "Calm down, I know what I'm doing!"
+      screeming?(String.trim(input)) -> "Whoa, chill out!"
+      question?(String.trim(input)) -> "Sure."
       true -> "Whatever."
     end
   end
 
-  defp is_question(input) do
-    String.graphemes(input) |> List.last() == "?"
+  defp question?(input) do
+    String.last(input) == "?"
   end
 
-  defp is_screeming(input) do
+  defp screeming?(input) do
     input == String.upcase(input) and input != String.downcase(input)
   end
 
